3c4871c36eeb5b5e0dad1fe369b8392ebc6e5e39
ZaeemUddinWork <zaeemuddinbus@gmail.com>
2024-03-06 19:53:32 +0500

    first commit

---
+++ Day 01/Task 03/main.ts
@@ -0,0 +1,30 @@
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+//this will same person name
+let person_name :string = 'ZaEeM uDdIn';
+
+//printing  name in lower case
+
+console.log(person_name.toLowerCase());
+
+//printing name into uppercase
+console.log(person_name.toUpperCase());
+
+//making function for titel case
+function titelcase(name)
+{
+    let word: string[] = name.split(" ");
+    
+    
+      const p_name = word[0].charAt(0).toUpperCase() + word[0].slice(1).toLowerCase() + ' ' +word[1].charAt(0).toUpperCase() + word[1].slice(1).toLowerCase()
+
+        
+    
+    console.log(p_name);
+}
+
+//printing titel case 
+titelcase(person_name)
+
+
+  
